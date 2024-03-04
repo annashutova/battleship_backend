9dbe543e08f751d8f8dcae909d47d6ab783b63f8
annashutova <annashut@inbox.ru>
2024-03-04 13:42:32 +0300

    added login api

---
+++ webapp/api/login/login.py
@@ -0,0 +1,29 @@
+from fastapi import Depends
+from fastapi.responses import ORJSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from webapp.api.login.router import auth_router
+from webapp.crud.user import get_user, create_user
+from webapp.db.postgres import get_session
+from webapp.schema.user import UserInfo, UserLoginResponse
+from webapp.utils.auth.jwt import jwt_auth
+
+
+@auth_router.post(
+    '/login',
+    response_model=UserLoginResponse,
+)
+async def login(
+    body: UserInfo,
+    session: AsyncSession = Depends(get_session),
+) -> ORJSONResponse:
+    user = await get_user(session, body)
+
+    if user is None:
+        user = await create_user(session, body)
+
+    return ORJSONResponse(
+        {
+            'access_token': jwt_auth.create_token(user.id),
+        }
+    )
